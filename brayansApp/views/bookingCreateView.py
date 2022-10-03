e7a568f81e3fb3e012653b926905712c9729be36
valentina03 <valentina03@gmail.com>
2022-10-02 23:53:20 -0500

    cambios autenticacion

--- brayansApp/views/bookingCreateView.py
+++ brayansApp/views/bookingCreateView.py
@@ -1,7 +1,10 @@
 from rest_framework import status, views, generics
 from rest_framework.response import Response
 from brayansApp.models.booking import Booking
-#from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.backends import TokenBackend
+from rest_framework.permissions import IsAuthenticated
+from django.conf import settings
 
 from brayansApp.serializers.bookingSerializer import BookingSerializer
 from brayansApp.serializers.clientSerializer import ClientSerializer
@@ -9,7 +12,7 @@ from brayansApp.serializers.clientSerializer import ClientSerializer
 class BookingListCreateView(generics.ListCreateAPIView):
     queryset = Booking.objects.all() #modelo de booking
     serializer_class = BookingSerializer
-    #permission_classes = (IsAuthenticated,) #se descomenta cuando termino de hacer pruebas
+    permission_classes = (IsAuthenticated,) #se descomenta cuando termino de hacer pruebas
 
     def list(self, request):
         print("GET a todos los Booking")
@@ -31,15 +34,15 @@ class BookingListCreateView(generics.ListCreateAPIView):
         serializerEnf = BookingSerializer(data=enfData)      
         serializerEnf.is_valid(raise_exception=True)
         serializerEnf.save()
-        return Response(status=status.HTTP_201_CREATED)
+        #return Response(status=status.HTTP_201_CREATED)
         
- #       tokenData = {
- #                      "username": request.data["username"],
- #                      "password": request.data["password"]
- #                      }
- #       tokenSerializer = TokenObtainPairSerializer(data=tokenData)
- #       tokenSerializer.is_valid(raise_exception=True)
- #       return Response(tokenSerializer.validated_data, status=status.HTTP_201_CREATED)
+        tokenData = {
+                       "username": request.data["username"],
+                       "password": request.data["password"]
+                       }
+        tokenSerializer = TokenObtainPairSerializer(data=tokenData)
+        tokenSerializer.is_valid(raise_exception=True)
+        return Response(tokenSerializer.validated_data, status=status.HTTP_201_CREATED)
  
 '''
     def post(self, request):
@@ -54,27 +57,37 @@ class BookingRetrieveUpddateView(generics.RetrieveUpdateAPIView): #obtener, actu
     serializer_class = BookingSerializer
     lookup_field = 'id' #campo con el que se realiza la busqueda de los objetos, el ID de la PK
     lookup_url_kwarg = 'pk' #nombre dado en la URL al argumento
-    #permission_classes = (isAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
         print("GET a Booking")
-        """if valid_data['user_id'] != kargs['pk'];
-        stringResponse = {'detail':'Unauthorized Request'}
-        return Response(stringResponse, status=status.HTTP_401_UNAUTORIZED) """
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token,verify=False)
+
+        if valid_data['user_id'] != kwargs['pk']:
+            stringResponse = {'detail':'Unauthorized Request'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTORIZED)
         return super().get(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
         print("PUT a Booking")
-        """ if valid_data['user_id] != kwargs['pk]:
-        stringResponse = {'detail':'Unauthorized Request'}
-        return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
-        """
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token,verify=False)
+        
+        if valid_data['user_id'] != kwargs['pk']:
+            stringResponse = {'detail':'Unauthorized Request'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTORIZED)
         return super().put(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         print("DELETE a Booking")
-        """ if valid_data['user_id] != kwargs['pk]:
-        stringResponse = {'detail':'Unauthorized Request'}
-        return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
-        """
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token,verify=False)
+
+        if valid_data['user_id'] != kwargs['pk']:
+            stringResponse = {'detail':'Unauthorized Request'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTORIZED)
         return super().delete(request, *args, **kwargs)
--- brayansApp/views/userDetailView.py
+++ brayansApp/views/userDetailView.py
@@ -10,16 +10,16 @@ from brayansApp.serializers.clientSerializer import ClientSerializer
 class UserDetailView(generics.RetrieveAPIView):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-    #permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
 
-        #token = request.META.get('HTTP_AUTHORIZATION')[7:]
-        #tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
-        #valid_data = tokenBackend.decode(token,verify=False)
+        token = request.META.get('HTTP_AUTHORIZATION')[7:]
+        tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
+        valid_data = tokenBackend.decode(token,verify=False)
 
-       # if valid_data['user_id'] != kwargs['pk']:
-            #stringResponse = {'detail':'Unauthorized Request'}
-           # return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+        if valid_data['user_id'] != kwargs['pk']:
+            stringResponse = {'detail':'Unauthorized Request'}
+            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
         
         return super().get(request, *args, **kwargs)
